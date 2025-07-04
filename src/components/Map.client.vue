8bda596600b8f488fbe16be6e0e2a51f892f200a
Sebastian Peters <koelnconcert@googlemail.com>
2025-07-04 17:02:54 +0200

    add map

--- src/components/Map.client.vue
+++ src/components/Map.client.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="w-screen h-screen">
+    <LMap :use-global-leaflet="false" v-model:zoom="zoom" v-model:center="center" :maxBounds="maxBounds"
+      :minZoom="minZoom" :maxZoom="maxZoom">
+      <LControlLayers/>
+      <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap" />
+      <template v-for="year in years" :key="year">
+        <LTileLayer :url="'/tiles/dop/' + year + '/{z}/{x}/{y}.png'" :tms="true" layer-type="overlay" :name="'DOP ' + year" />
+      </template>
+    </LMap>
   </div>
 </template>
 
 <script lang="ts" setup>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LControlLayers } from "@vue-leaflet/vue-leaflet"
+
+const zoom = ref(12)
+const minZoom = ref(12)
+const maxZoom = ref(19)
+const center = ref([52.4, 9.7])
+const maxBounds = ref([[52.2, 9.6], [53, 10]])
+
+const years = ref([1957, 1965, 1977, 2023])
 </script>
