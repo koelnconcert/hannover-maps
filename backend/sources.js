ed2ca0bb4404953c6fe2f3c1be70dc6afa2e6688
Sebastian Peters <koelnconcert@googlemail.com>
2025-07-19 00:09:40 +0200

    extract source information into source.js

---
+++ backend/sources.js
@@ -0,0 +1,38 @@
+const openGeoDataWebsiteBaseUrl = 'https://www.hannover.de/Leben-in-der-Region-Hannover/Verwaltungen-Kommunen/Die-Verwaltung-der-Landeshauptstadt-Hannover/Dezernate-und-Fachbereiche-der-LHH/Stadtentwicklung-und-Bauen/Fachbereich-Planen-und-Stadtentwicklung/Geoinformation/Open-GeoData'
+const dopWebsiteBaseUrl = openGeoDataWebsiteBaseUrl + '/Digitale-Orthophotos-DOP20'
+const downloadBaseUrl = 'https://opengeodata.hannover-stadt.de'
+
+const YEARS = [1957, 1965, 1977, 1981, 1991, 2002, 2006, 2015, 2021, 2023]
+const PARTS = ['Nord']
+
+const arrayToObject = (array, mapper) => Object.fromEntries(array.map(key => [key, mapper(key)]))
+
+const licenses = {
+  'CC-BY-4.0': {
+    name: 'Creative Commons Namensnennung 4.0 DE',
+    url: 'https://creativecommons.org/licenses/by/4.0/deed.de'
+  }
+}
+
+const defaultLicenseHolder = 'Landeshauptstadt Hannover'
+
+const sources = {
+  dop: {
+    name: 'Digitale Orthophotos (DOP20)',
+    website: dopWebsiteBaseUrl,
+    years: arrayToObject(YEARS, year => ({
+      website: dopWebsiteBaseUrl + '/' +
+        ((year === 2023) ? 'Digitale' : 'Historische') + '-Orthophotos-Jahrgang-' + year,
+      license: {
+        ...licenses['CC-BY-4.0'],
+        holder: defaultLicenseHolder
+      },
+      parts: arrayToObject(PARTS, part => ({
+        downloadUrl: downloadBaseUrl + '/' +
+          ((year === 2023) ? `DOP20_Teil_${part}.zip` : `${year}_${part}.zip`),
+      }))
+    }))
+  }
+}
+
+export default sources
